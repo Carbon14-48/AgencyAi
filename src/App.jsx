a0787baa7aae905ab2298fa7d179e27d6a314eab
Carbon14-48 <mohammedberrichi4@gmail.com>
2025-08-28 15:05:10 +0100

    form Added

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import TrustedBy from "./components/TrustedBy";
 import Services from "./components/Services";
 import OurWork from "./components/OurWork";
 import Teams from "./components/Teams";
+import ContactUs from "./components/ContactUs";
 function App() {
   const [theme, setTheme] = useState(() => {
     const savedTheme = localStorage.getItem("theme");
@@ -32,6 +33,7 @@ function App() {
       <Services />
       <OurWork />
       <Teams />
+      <ContactUs />
     </div>
   );
 }
